0592d09fd28b8e65775c0fabba306c4e18c148ec
wswlfh <91122182+wswlfh@users.noreply.github.com>
2023-06-14 16:20:02 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
+  let target = "https://www.github.com/";
 
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
     },
   })(req, res);
 };
