9b9732ec7c556264ce6703d192cd7e38e05732df
wswlfh <91122182+wswlfh@users.noreply.github.com>
2023-06-14 16:23:53 +0800

    update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.github.com/";
+  let target = "https://www.reddit.com/";
 
   createProxyMiddleware({
     target,
